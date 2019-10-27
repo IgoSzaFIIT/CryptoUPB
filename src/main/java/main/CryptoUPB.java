0a2e4331bb037699ce8cf38fe298a62742ca5c59
Juraj Kominak <kominakjuraj@gmail.com>
2019-10-28 00:34:12 +0100

    Pridany generator Keypairov, Pridany vypocet MAC Mac pripraveny na kontrolu

--- src/main/java/main/CryptoUPB.java
+++ src/main/java/main/CryptoUPB.java
@@ -54,6 +54,7 @@ public class CryptoUPB {
                 byte[] mac = generateMACFromMessage(key, outputBytes);
 
                 System.out.println("MAC: " + mac);
+                System.out.println("dlzka MAC : "  + mac.length);
 
 
                 byte[] publicBytes = Base64.getDecoder().decode(RSAKey);//.decodeBase64(publicK);
@@ -85,10 +86,11 @@ public class CryptoUPB {
                 System.out.println("iv_bytes = " + IV.length);
 
                 //vysledok kde je spojena hlavicka, kde je zasifrovany kluc a zvysok je zasifrovany text
-                byte[] AES_output = new byte[RSA_output.length + outputBytes.length];
+                byte[] AES_output = new byte[mac.length + RSA_output.length + outputBytes.length];
 
-                System.arraycopy(RSA_output, 0, AES_output, 0, RSA_output.length);
-                System.arraycopy(outputBytes, 0, AES_output, RSA_output.length, outputBytes.length);
+                System.arraycopy(mac, 0, AES_output, 0, mac.length);
+                System.arraycopy(RSA_output, 0, AES_output, mac.length, RSA_output.length);
+                System.arraycopy(outputBytes, 0, AES_output, mac.length + RSA_output.length, outputBytes.length);
 
                 return  AES_output;
 
@@ -124,16 +126,21 @@ public class CryptoUPB {
                 Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
                 cipher.init(Cipher.DECRYPT_MODE, privateKey);
 
-                byte[] ct_RSA = Arrays.copyOfRange(fileContent, 0, 256);
+                byte[] ct_RSA = Arrays.copyOfRange(fileContent, 0+32, 256+32);
                 System.out.println("cyphertext " + new String(ct_RSA) + " dd " + ct_RSA.length );
                 byte[] Key_IV = cipher.doFinal(ct_RSA);
 
 
                 System.out.println("jkey " + new String(Key_IV) );
+
+                //mac pripraveny na kontrolu
+                byte[] mac = Arrays.copyOfRange(fileContent, 0, 32);
+
+
                 byte[] key = Arrays.copyOfRange(Key_IV, 0, 16);
                 byte[] IV = Arrays.copyOfRange(Key_IV, 16, 32);
 
-                fileContent = Arrays.copyOfRange(fileContent, 256, fileContent.length);
+                fileContent = Arrays.copyOfRange(fileContent, 256+32, fileContent.length);
                 cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
                 // Create SecretKeySpec
