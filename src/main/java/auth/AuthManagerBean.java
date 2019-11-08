5aba452478a93dc155907d1722a30f4239bfd07b
Ejdrijen <a.blazicek@gmail.com>
2019-11-09 00:38:27 +0100

    saltenie hesla pri registracii, hashovanie hesla, caosvy rozostup medzi prihlaseniami, zlozitost hesla - male velke pismeno, cislo

--- src/main/java/auth/AuthManagerBean.java
+++ src/main/java/auth/AuthManagerBean.java
@@ -1,37 +1,45 @@
 package auth;
 
 import java.io.File;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.SecretKeyFactory;
+
 
 @ApplicationScoped
 @ManagedBean(name="authManagerBean")
 public class AuthManagerBean {
-    
+
     private final String INDEX_PATH = "/index.xhtml";
     private final String LOGIN_PATH = "/auth/login.xhtml";
     private final String SAVE_FOLDER = "\\temp\\";
     private final String USERS_TABLE_NAME = "users";
-    
+
     //SQLite DB
     private Connection dbConn;
-    
+
     //username
     private String usr;
-    
+
     //password
     private String pwd;
-    
+
     //password repeat (used in registation)
     private String pwdR;
 
@@ -42,11 +50,11 @@ public class AuthManagerBean {
     public String getPwd() {
         return pwd;
     }
-    
+
     public String getPwdR() {
         return pwdR;
-    }    
-    
+    }
+
     public void setUsr(String usr) {
         this.usr = usr;
     }
@@ -58,22 +66,21 @@ public class AuthManagerBean {
     public void setPwdR(String pwdR) {
         this.pwdR = pwdR;
     }
-    
-    
+
+
     //handle user login
     public String handleLogin() {
         if(usr.length() < 1) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Please enter your username."));
             return null;
-        }        
+        }
         if(pwd.length() < 1) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Please enter your password."));
             return null;
         }
         /*
             TODO:
-            Take 'pwd' (user password in plain text) and create a hash.
-            Then save it in 'pwdhash'.
+            potrebujeme z DB vytiahnut salt pre daneho uzivatela
         */
         String pwdhash = pwd;
         int nAttempts = 0;
@@ -83,7 +90,7 @@ public class AuthManagerBean {
             if(res.isBeforeFirst()) {
                 nAttempts = res.getInt("loginAttempts");
                 lastAttempt = res.getTimestamp("lastAttempt");
-                
+
                 /* Lockout for 5 minutes after 3 unsuccessful attempts */
                 if(nAttempts > 2){
                     if((java.sql.Timestamp.from(java.time.Instant.now()).getTime() - lastAttempt.getTime()) > 300000)
@@ -95,18 +102,23 @@ public class AuthManagerBean {
                         return null;
                     }
                 }
-                    
+
             }
-            
+
         }
         catch(Exception ex) {
             ex.printStackTrace();
         }
-        
+
         boolean valid = validateLogin(this.usr, pwdhash);
         nAttempts++;
         if(!valid) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Username/password combination is incorrect."));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             DBUtils.updateLoginAttempts(dbConn, USERS_TABLE_NAME, usr, nAttempts);
             return null;
         }
@@ -118,24 +130,24 @@ public class AuthManagerBean {
             return INDEX_PATH + "?faces-redirect=true";
         }
     }
-    
+
     //handle user logout
     public String handleLogout() {
         HttpSession session = SessionUtils.getSession();
         session.invalidate();
         return LOGIN_PATH + "?faces-redirect=true";
     }
-    
+
     //handle user registration
     public String handleRegistration() {
         if(usr.length() < 1) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Please enter a username."));
             return null;
-        }        
+        }
         if(pwd.length() < 1) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Please enter a password."));
             return null;
-        }        
+        }
         if(pwdR.length() < 1) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Please repeat the password."));
             return null;
@@ -144,19 +156,49 @@ public class AuthManagerBean {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Passwords do not match!"));
             return null;
         }
-        /*
-            TODO:
-            Take 'pwd' (user password in plain text) and create a hash.
-            Then save it in 'pwdhash'.
-        */
-        String pwdhash = pwd;
+        if(pwd.length() < 6) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Password too short."));
+            return null;
+        }
+        Pattern regex = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$");
+        if(!regex.matcher(pwd).matches()){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Password must contain at least one uppercase letter, one lowercase letter and one digit."));
+            return null;
+        }
+        //TODO:
+        // overenie ci je heslo slovnikove
+        byte[] salt = salt(8);
+        String pwdhash = null;
+        try {
+            pwdhash = Hash(pwd,salt,126,65536);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        //TODO: treba ukladat aj salt ( meno, hash, salt )
         DBUtils.insertUser(dbConn, USERS_TABLE_NAME, usr, pwdhash);
-        
+
         HttpSession session = SessionUtils.getSession();
         session.setAttribute("username", usr);
         return handleLogin();
     }
-    
+
+    public String Hash(String pwd, byte[] salt, int lengthBits, int iterCount) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //hash
+        KeySpec spec = new PBEKeySpec(pwd.toCharArray(), salt, iterCount, lengthBits);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        String pwdhash = factory.generateSecret(spec).getEncoded().toString();
+        return pwdhash;
+    }
+
+    private byte[] salt(int sizeInBytes){
+        SecureRandom rnd = new SecureRandom();
+        byte[] salt = new byte[sizeInBytes];
+        rnd.nextBytes(salt);
+        return salt;
+    }
+
     private boolean validateLogin(String u, String p) {
         /*
             Check if user is already logged in
@@ -167,7 +209,7 @@ public class AuthManagerBean {
             if(usrS.equals(usr))
                 return true;
         }
-        
+
         ResultSet res = DBUtils.selectUser(dbConn, USERS_TABLE_NAME, u, p);
         try {
             //wrong login
@@ -176,15 +218,15 @@ public class AuthManagerBean {
             //correct login
             else
                 return true;
-            
+
         }
         catch(Exception ex) {
             ex.printStackTrace();
         }
-        
+
         return false;
     }
-    
+
     @PostConstruct
     public void init() {
         /* Initialize SQLite DB connection */
@@ -192,7 +234,7 @@ public class AuthManagerBean {
         String dbFileName = "users.db";
         dbConn = DBUtils.initDB(dbPath, dbFileName);
     }
-    
-    
-    
+
+
+
 }
